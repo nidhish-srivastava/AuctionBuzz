ab79a60cf7035f99046c5b8f78a3f9eb04a0a454
Nidhish Srivastava <nidhish0430@gmail.com>
2024-04-07 15:44:27 +0530

    Login,Signup ui auth modals done

---
+++ components/navbars/Navbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { SafeUser } from "@/lib/types";
+import Container from "../ui/Container";
+import UserMenu from "./UserMenu";
+
+type NavbarProps = {
+  user: SafeUser | null;
+};
+
+function Navbar({ user }: NavbarProps) {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            LOGO
+            <input type="search" />
+            <UserMenu currentUser={user}></UserMenu>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**.githubusercontent.com",
+          port: "",
+        },
+        {
+          protocol: "https",
+          hostname: "**.googleusercontent.com",
+          port: "",
+        },
+        {
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+          port: "",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
